8ed987ac7ee6429fc5311d1250fa354bf325e202
Maarten Flippo <maartenflippo@outlook.com>
2025-06-20 09:44:19 +0200

    Show hidden files in oil by default

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,14 +1,18 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-	  keymaps = {
-		  ["<ESC>"] = "actions.close",
-	  }
-  },
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		watch_for_changes = true,
+		keymaps = {
+			["<ESC>"] = "actions.close",
+		},
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
 }
