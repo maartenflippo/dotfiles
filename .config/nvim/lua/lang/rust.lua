d91d561b850675f188b73d7e59f6f04c271bb1d9
Maarten Flippo <maartenflippo@outlook.com>
2026-04-22 10:30:49 +1000

    [Neovim] Add tree-sitter-rust parser

--- .config/nvim/lua/lang/rust.lua
+++ .config/nvim/lua/lang/rust.lua
@@ -1,1 +1,10 @@
+vim.treesitter.language.register("rust", "rust")
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "rust",
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
+
 vim.lsp.enable("rust_analyzer")
