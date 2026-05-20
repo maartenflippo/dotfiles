6bc3e589f3ffabfe285febf21a5abe08ed60c1dc
Maarten Flippo <maartenflippo@outlook.com>
2026-05-20 11:42:11 +1000

    [Neovim] Build rust projects with all features enabled

--- .config/nvim/lua/lang/rust.lua
+++ .config/nvim/lua/lang/rust.lua
@@ -1,3 +1,13 @@
 require("treesitter").enable("rust")
 
+vim.lsp.config('rust_analyzer', {
+    settings = {
+        ['rust-analyzer'] = {
+            cargo = {
+                allFeatures = true,
+            },
+        },
+    },
+})
+
 vim.lsp.enable("rust_analyzer")
