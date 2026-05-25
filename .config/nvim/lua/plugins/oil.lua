b7a862c4aeb30d58e19f9e3977318b42acdb41b8
Maarten Flippo <maartenflippo@outlook.com>
2026-05-25 10:29:41 +1000

    [Neovim] Show hidden files by default in Oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,4 +1,8 @@
-require("oil").setup()
+require("oil").setup({
+    view_options = {
+        show_hidden = true,
+    },
+})
 
 -- If Oil is installed, we override the builtin Netrw explorer with Oil.
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
